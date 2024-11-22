ea4498534858e5534df122eddc904d9a48940187
Nievas2 <angelgabrielnievas@gmail.com>
2024-11-22 14:30:19 -0300

    feat(General): :sparkles: Add contexts, Add default pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,58 @@
+import { Navigate, Route, Routes, useLocation } from "react-router-dom"
+import { useAuthContext } from "./contexts/auth-context"
+import HomePage from "./pages/home/HomePage"
+import LoginPage from "./pages/login/LoginPage"
+import RegisterPage from "./pages/register/RegisterPage"
+import NotFoundPage from "./pages/notFound/NotFoundPage"
+import Footer from "./components/shared/Footer"
+import Navbar from "./components/shared/Navbar"
 function App() {
+  const location = useLocation()
+  const noPadding = ["/", "/login", "/signup", "/home"].includes(
+    location.pathname
+  )
+  const { authUser } = useAuthContext()
+
   return (
-    <>
-      <h1>ZeilSoft</h1>
-    </>
+    <main className="bg-light dark:bg-dark text-dark dark:text-light">
+      <section className="w-full font-sigmar flex flex-col justify-center items-center min-h-screen">
+        <Navbar />
+
+        <div
+          className={`flex flex-1 max-w-8xl ${!noPadding ? "py-7 px-4" : ""}`}
+        >
+          <Routes location={location} key={location.pathname}>
+            {/* RUTAS NO PROTEGIDAS */}
+            <Route path="/" element={<HomePage />} />
+
+            {/* RUTAS PROTEGIDAS */}
+            {/* <Route
+              path="/favorites"
+              element={
+                <ProtectedRoute>
+                  <FavoritesPage />
+                </ProtectedRoute>
+              }
+            /> */}
+
+            <Route
+              path="/login"
+              element={authUser ? <Navigate to="/home" /> : <LoginPage />}
+            />
+            {/* <Route path="/login/auth" element={<AuthPage />} /> */}
+            <Route
+              path="/signup"
+              element={authUser ? <Navigate to="/home" /> : <RegisterPage />}
+            />
+
+            <Route path="/404" element={<NotFoundPage />} />
+            <Route path="/*" element={<Navigate to="/404" replace />} />
+          </Routes>
+        </div>
+
+        <Footer />
+      </section>
+    </main>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,24 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ThemeProvider } from "./contexts/theme-provider.tsx"
+import { BrowserRouter } from "react-router-dom"
+import { AuthContextProvider } from "./contexts/auth-context.tsx"
 import App from "./App.tsx"
 import "./index.css"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
 const queryClient = new QueryClient()
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <BrowserRouter>
+          <AuthContextProvider>
+            <App />
+          </AuthContextProvider>
+        </BrowserRouter>
+      </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>
 )
