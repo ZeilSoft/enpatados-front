bb8ff18689ad9dc4921840bc821ddea40b555961
Nievas2 <angelgabrielnievas@gmail.com>
2024-12-20 10:23:06 -0300

    fix(Dashboard): :bug: Fix welcome

--- src/dashboardAdmin/components/Sidebar.tsx
+++ src/dashboardAdmin/components/Sidebar.tsx
@@ -1,75 +1,105 @@
-import { Link } from 'react-router-dom';
-import { SidebarItem } from './SidebarItem';
-import { ShoppingCart, Tag, Users, LucideIcon, Box, LogOut, ChevronLeft } from 'lucide-react';
-import { Button } from '@/components/ui/button';
+import { Link } from "react-router-dom"
+import { SidebarItem } from "./SidebarItem"
+import {
+  ShoppingCart,
+  Tag,
+  Users,
+  LucideIcon,
+  Box,
+  LogOut,
+  ChevronLeft,
+} from "lucide-react"
+import { Button } from "@/components/ui/button"
+import { useAuthContext } from "@/auth/context/auth-context"
 
 interface MenuItem {
-  title    : string;
-  subTitle : string;
-  href     : string;
-  Icon     : LucideIcon;
+  title: string
+  subTitle: string
+  href: string
+  Icon: LucideIcon
 }
 
 const menuItems: MenuItem[] = [
-  { title: 'Productos', subTitle: 'Gestión de inventario', href: '/admin/productos', Icon: Box },
-  { title: 'Clasificación', subTitle: 'Administrar categorias', href: '/admin/categorias', Icon: Tag },
-  { title: 'Ordenes de compra', subTitle: 'Estados de pago', href: '/admin/ordenes-de-compra', Icon: ShoppingCart },
-  { title: 'Usuarios', subTitle: 'Lista de usuarios', href: '/admin/usuarios', Icon: Users },
-];
+  {
+    title: "Productos",
+    subTitle: "Gestión de inventario",
+    href: "/admin/productos",
+    Icon: Box,
+  },
+  {
+    title: "Clasificación",
+    subTitle: "Administrar categorias",
+    href: "/admin/categorias",
+    Icon: Tag,
+  },
+  {
+    title: "Ordenes de compra",
+    subTitle: "Estados de pago",
+    href: "/admin/ordenes-de-compra",
+    Icon: ShoppingCart,
+  },
+  {
+    title: "Usuarios",
+    subTitle: "Lista de usuarios",
+    href: "/admin/usuarios",
+    Icon: Users,
+  },
+]
 
 export const Sidebar = () => {
-
+  const { authUser } = useAuthContext()
   return (
     <div className="hidden lg:border-r lg:border-[#334155] bg-[#252D3B] h-screen relative lg:flex flex-col justify-between z-[8888] text-slate-300 w-80 left-0">
-
       {/* Intro & Profile */}
       <div className="flex flex-col gap-2 pt-4 px-4">
-
         {/* Intro */}
         <div>
           <h1 className="text-lg md:text-2xl font-bold text-white">
             Enpatados
           </h1>
 
-          <span className="text-blue-500 text-sm font-bold">Panel administrativo</span>
+          <span className="text-blue-500 text-sm font-bold">
+            Panel administrativo
+          </span>
           {/* <p className="text-slate-500 text-sm">Manejador de estados simple pero poderoso.</p> */}
         </div>
 
         {/* Profile */}
-        <div className="flex flex-col"> 
+        <div className="flex flex-col">
           <p className="text-slate-500">Bienvenido,</p>
           <div className="inline-flex space-x-2 items-center">
             {/* <img className="rounded-full w-8 h-8" src="/logo.webp" alt="" /> */}
             <span className="text-sm md:text-base font-bold">
-              Santiago Herrera
+              {authUser?.user.email}
             </span>
           </div>
         </div>
       </div>
 
-      {/* Menu Items */ }
+      {/* Menu Items */}
       <nav className="w-full flex flex-col">
-        {
-          menuItems.map( item =>(
-            <SidebarItem key={item.href} {...item} />
-          ) )
-        }
+        {menuItems.map((item) => (
+          <SidebarItem key={item.href} {...item} />
+        ))}
       </nav>
-      
+
       {/* Go back & Logout */}
-      <div className='flex flex-col justify-center gap-2 px-4 pb-4'>
+      <div className="flex flex-col justify-center gap-2 px-4 pb-4">
         {/* Go Home */}
-        <Link className='flex gap-1 items-center justify-center whitespace-nowrap text-sm font-medium rounded-md transition-colors hover:bg-[#111827] h-10 px-4 py-2 border border-[#334155] focus-visible:outline-none focus-visible:ring-2 focus:ring-white' to="/">
+        <Link
+          className="flex gap-1 items-center justify-center whitespace-nowrap text-sm font-medium rounded-md transition-colors hover:bg-[#111827] h-10 px-4 py-2 border border-[#334155] focus-visible:outline-none focus-visible:ring-2 focus:ring-white"
+          to="/"
+        >
           <ChevronLeft />
           <span className="">Volver al Inicio</span>
         </Link>
 
         {/* Logout */}
-        <Button variant="logOut" className='flex flex-row gap-2 items-center'>
+        <Button variant="logOut" className="flex flex-row gap-2 items-center">
           <LogOut />
           Cerrar sesion
         </Button>
       </div>
     </div>
-  );
-};
+  )
+}
--- src/dashboardAdmin/components/SidebarMobile.tsx
+++ src/dashboardAdmin/components/SidebarMobile.tsx
@@ -1,63 +1,98 @@
-import { Link, NavLink } from 'react-router-dom';
-import { ShoppingCart, Tag, Users, Box, LogOut, LucideIcon, ChevronLeft } from 'lucide-react';
+import { Link, NavLink } from "react-router-dom"
+import {
+  ShoppingCart,
+  Tag,
+  Users,
+  Box,
+  LogOut,
+  LucideIcon,
+  ChevronLeft,
+} from "lucide-react"
 
 interface MenuItem {
-  title: string;
-  href: string;
-  Icon: LucideIcon;
+  title: string
+  href: string
+  Icon: LucideIcon
 }
 
 const menuItems: MenuItem[] = [
-  { title: 'Productos', href: '/admin/productos', Icon: Box },
-  { title: 'Clasificación', href: '/admin/categorias', Icon: Tag },
-  { title: 'Órdenes', href: '/admin/ordenes-de-compra', Icon: ShoppingCart },
-  { title: 'Usuarios', href: '/admin/usuarios', Icon: Users },
-];
+  { title: "Productos", href: "/admin/productos", Icon: Box },
+  { title: "Clasificación", href: "/admin/categorias", Icon: Tag },
+  { title: "Órdenes", href: "/admin/ordenes-de-compra", Icon: ShoppingCart },
+  { title: "Usuarios", href: "/admin/usuarios", Icon: Users },
+]
 
-import { useSidebarStore } from "@/store/ui.store";
-import { Button } from '@/components/ui/button';
+import { useSidebarStore } from "@/store/ui.store"
+import { Button } from "@/components/ui/button"
+import { useAuthContext } from "@/auth/context/auth-context"
 
 export const SidebarMobile = () => {
-  const { isMobileSidebarOpen, toggleSidebar } = useSidebarStore();
+  const { isMobileSidebarOpen, toggleSidebar } = useSidebarStore()
+  const { authUser } = useAuthContext()
 
   return (
     <div
-      className={`lg:hidden z-[8888] absolute bg-[#111827] h-screen flex flex-col justify-between text-white transition-all duration-300 ease-in-out overflow-hidden ${isMobileSidebarOpen ? 'w-64 p-4' : 'w-0 py-4'}`}
+      className={`lg:hidden z-[8888] absolute bg-[#111827] h-screen flex flex-col justify-between text-white transition-all duration-300 ease-in-out overflow-hidden ${
+        isMobileSidebarOpen ? "w-64 p-4" : "w-0 py-4"
+      }`}
     >
-
       {/* Navbar Items y Profile */}
       <div
-        className={`flex flex-col gap-20 transition-opacity duration-300 ease-in-out ${isMobileSidebarOpen ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-[-100%]'}`}
+        className={`flex flex-col gap-20 transition-opacity duration-300 ease-in-out ${
+          isMobileSidebarOpen
+            ? "opacity-100 translate-x-0"
+            : "opacity-0 translate-x-[-100%]"
+        }`}
       >
         {/* Profile */}
         <div className="flex flex-col gap-2 mt-16">
           {/* Intro */}
           <div>
-            <h1 className={`text-lg md:text-2xl font-bold text-white text-nowrap ${isMobileSidebarOpen ? 'opacity-100' : 'opacity-0'}`}>
+            <h1
+              className={`text-lg md:text-2xl font-bold text-white text-nowrap ${
+                isMobileSidebarOpen ? "opacity-100" : "opacity-0"
+              }`}
+            >
               Enpatados
             </h1>
 
-            <span className={`text-blue-500 text-sm font-bold text-nowrap ${isMobileSidebarOpen ? 'opacity-100' : 'opacity-0'}`}>Panel administrativo</span>
+            <span
+              className={`text-blue-500 text-sm font-bold text-nowrap ${
+                isMobileSidebarOpen ? "opacity-100" : "opacity-0"
+              }`}
+            >
+              Panel administrativo
+            </span>
           </div>
 
           {/* User */}
           <div className="flex flex-col">
-            <p className={`text-slate-500 text-nowrap ${isMobileSidebarOpen ? 'opacity-100' : 'opacity-0'}`}>Bienvenido,</p>
+            <p
+              className={`text-slate-500 text-nowrap ${
+                isMobileSidebarOpen ? "opacity-100" : "opacity-0"
+              }`}
+            >
+              Bienvenido,
+            </p>
             <div className="inline-flex space-x-2 items-center">
               {/* <img className="rounded-full w-8 h-8" src="/logo.webp" alt="" /> */}
-              <span className={`text-sm md:text-base font-bold text-nowrap ${isMobileSidebarOpen ? 'opacity-100' : 'opacity-0'}`}>
-                Santiago Herrera
+              <span
+                className={`text-sm md:text-base font-bold text-nowrap ${
+                  isMobileSidebarOpen ? "opacity-100" : "opacity-0"
+                }`}
+              >
+                {authUser?.user.email}
               </span>
             </div>
           </div>
         </div>
 
-        <nav className='flex flex-col gap-3'>
+        <nav className="flex flex-col gap-3">
           {menuItems.map((item) => (
             <NavLink
-              to={item.href} 
+              to={item.href}
               key={item.title}
-              className={({ isActive }) => 
+              className={({ isActive }) =>
                 `${"w-full flex flex-row gap-3 items-center p-2 lg:border-b rounded transition-colors ease-linear duration-700"} 
                 ${isActive ? "bg-[#252D3B]" : "hover:bg-white/5"}`
               }
@@ -71,19 +106,26 @@ export const SidebarMobile = () => {
       </div>
 
       {/* Go Home and Log out */}
-      <div className={`flex flex-col justify-center gap-4 transition-opacity duration-300 ease-in-out ${isMobileSidebarOpen ? 'opacity-100' : 'opacity-0'}`}>
+      <div
+        className={`flex flex-col justify-center gap-4 transition-opacity duration-300 ease-in-out ${
+          isMobileSidebarOpen ? "opacity-100" : "opacity-0"
+        }`}
+      >
         {/* Go Home */}
-        <Link className='flex gap-1 items-center justify-center whitespace-nowrap text-sm font-medium rounded-md transition-colors hover:bg-[#111827] h-10 px-4 py-2 border border-[#334155] focus-visible:outline-none focus-visible:ring-2 focus:ring-white' to="/">
+        <Link
+          className="flex gap-1 items-center justify-center whitespace-nowrap text-sm font-medium rounded-md transition-colors hover:bg-[#111827] h-10 px-4 py-2 border border-[#334155] focus-visible:outline-none focus-visible:ring-2 focus:ring-white"
+          to="/"
+        >
           <ChevronLeft />
           <span className="">Volver al Inicio</span>
         </Link>
 
         {/* Logout */}
-        <Button variant="logOut" className='flex flex-row gap-2 items-center'>
+        <Button variant="logOut" className="flex flex-row gap-2 items-center">
           <LogOut />
           Cerrar sesion
         </Button>
       </div>
     </div>
-  );
-};
+  )
+}
